d57872dd624c876a4f655be2b95610e544500366
Philipp A. <flying-sheep@web.de>
2023-12-12 16:34:29 +0100

    “fix” tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -50,7 +50,7 @@ def test_load_plugin() -> None:
 def test_basic(basic_project: Path) -> None:
     hook = mk_hook(basic_project)
     version_api = hook.get_version_api()
-    assert version_api.keys() == {"1.0"}
+    assert version_api.keys() == {"in-tree"}
     # TODO: implement
     # https://github.com/flying-sheep/hatch-scanpy-typeshed/issues/3
     pytest.xfail("TODO")
