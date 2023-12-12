85e5e9070a86d6423b662aef26a6026b752f58ba
Philipp A. <flying-sheep@web.de>
2023-12-12 16:35:53 +0100

    3.10 compat

--- src/hatch_scanpy_typeshed/transform.py
+++ src/hatch_scanpy_typeshed/transform.py
@@ -4,13 +4,14 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from itertools import chain
-from typing import TYPE_CHECKING, Any, Never
+from typing import TYPE_CHECKING
 from warnings import warn
 
 from mypy.stubdoc import ArgSig, FunctionSig
 
 if TYPE_CHECKING:
     from collections.abc import Generator, Iterable, Iterator
+    from typing import Any, Never
 
 
 @dataclass
